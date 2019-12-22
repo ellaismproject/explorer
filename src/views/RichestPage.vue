99069ae60df7d12d4b83713210f0b2565572a5b0
majordutch <354294+majordutch@users.noreply.github.com>
2019-12-21 23:24:18 -0800

    Simplify rich list view and add pagination

--- src/views/RichestPage.vue
+++ src/views/RichestPage.vue
@@ -1,32 +1,42 @@
 import {SortType} from "@/models/SortType";
 <template>
-    <div v-if="isLoading">
-        <PageLoader/>
-    </div>
-    <div v-else-if="addresses !== null" class="richest-page">
+    <div class="richest-page">
         <h1 class="title is-4 is-spaced">{{ $t('page.richest.title') }}</h1>
-        <RichestList :addresses="addresses"/>
+        <b-table class="is-striped is-hoverable is-fullwidth" :data="addresses" paginated backend-pagination
+                 @page-change="onPageChange" :total="total" :per-page="perPage" icon-pack="fas" :loading="isLoading">
+            <template slot-scope="props">
+                <b-table-column field="rank" label="Rank">
+                    {{ props.row.rank }}
+                </b-table-column>
+                <b-table-column field="hash" label="Address">
+                    <router-link :to="{ name: 'address', params: { hash: props.row.hash }}">{{ props.row.hash }}
+                    </router-link>
+                </b-table-column>
+                <b-table-column field="balance" label="Balance">
+                    {{ props.row.balance }}
+                </b-table-column>
+            </template>
+            <template slot="empty">
+                <section class="section">
+                    <div class="content has-text-grey has-text-centered">
+                        <p>
+                            <b-icon pack="fas" icon="coins" size="is-large"/>
+                        </p>
+                        <p>No addresses found.</p>
+                    </div>
+                </section>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import RichestList from '@/components/RichestList.vue';
-    import PageLoader from '@/components/PageLoader.vue';
     import CinderApiService from '@/services/CinderApiService';
-    import store from '@/Store';
-    import {mapState} from 'vuex';
     import Richest from '@/models/Richest';
     import {MetaInfo} from 'vue-meta';
 
     @Component({
-        components: {
-            PageLoader,
-            RichestList,
-        },
-        computed: mapState([
-            'isLoading',
-        ]),
         metaInfo(): MetaInfo {
             return {
                 title: this.$t('page.richest.meta_title').toString(),
@@ -35,22 +45,33 @@ import {SortType} from "@/models/SortType";
         },
     })
     export default class RichestPage extends Vue {
-        public isLoading!: boolean;
-        public addresses: Richest[] | null = null;
+        public currentPage: number = 1;
+        public perPage: number = 100;
+        public total: number = 0;
+        public addresses: Richest[] = [];
+        public isLoading: boolean = false;
         private api = new CinderApiService();
 
         public async created() {
+            this.isLoading = true;
             await this.getRichest();
+            this.isLoading = false;
+        }
+
+        public async onPageChange(page: number) {
+            this.isLoading = true;
+            this.currentPage = page;
+            await this.getRichest();
+            this.isLoading = false;
         }
 
         private async getRichest(): Promise<void> {
-            store.commit('toggleLoader', true);
             try {
-                const data = await this.api.getRichest(1, 100);
+                const data = await this.api.getRichest(this.currentPage, this.perPage);
+                this.total = data.total;
                 this.addresses = data.items;
             } catch (e) {
             }
-            store.commit('toggleLoader', false);
         }
     }
 </script>
