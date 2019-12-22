0aca9fa601966bf8d396b267c505a13a02b61419
majordutch <354294+majordutch@users.noreply.github.com>
2019-12-21 23:24:54 -0800

    Simplify home page and add pagination

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,47 +1,80 @@
 import {SortType} from "@/models/SortType";
 <template>
-    <div v-if="isLoading">
-        <PageLoader/>
-    </div>
-    <div v-else class="index-page">
+    <div class="home-page">
         <h1 class="title is-4 is-spaced">{{ $t('page.home.title') }}</h1>
-        <BlockList :blocks="blocks"/>
+        <b-table class="is-striped is-hoverable is-fullwidth" :data="blocks" paginated backend-pagination
+                 @page-change="onPageChange" :total="total" :per-page="perPage" icon-pack="fas" :loading="isLoading">
+            <template slot-scope="props">
+                <b-table-column field="blockNumber" label="Height">
+                    <router-link :to="{ name: 'block', params: { hash: props.row.hash }}">{{ props.row.blockNumber }}
+                    </router-link>
+                </b-table-column>
+                <b-table-column field="miner" label="Relayed By">
+                    <router-link :to="{ name: 'address', params: { hash: props.row.miner }}">
+                        {{ props.row.minerDisplay || 'Unknown' }}
+                    </router-link>
+                </b-table-column>
+                <b-table-column field="timestamp" label="Age">
+                    {{ moment.unix(props.row.timestamp).fromNow() }}
+                </b-table-column>
+                <b-table-column field="transactionCount" label="Transactions" numeric>
+                    {{ props.row.transactionCount }}
+                </b-table-column>
+                <b-table-column field="size" label="Size" numeric>
+                    {{ props.row.size }}
+                </b-table-column>
+                <b-table-column field="hash" label="Block Hash">
+                    <router-link :to="{ name: 'block', params: { hash: props.row.hash }}"
+                                 class="is-extra-large-hash text-truncate">{{ props.row.hash }}
+                    </router-link>
+                </b-table-column>
+            </template>
+            <template slot="empty">
+                <section class="section">
+                    <div class="content has-text-grey has-text-centered">
+                        <p>
+                            <b-icon pack="fas" icon="cubes" size="is-large"/>
+                        </p>
+                        <p>No blocks found.</p>
+                    </div>
+                </section>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import BlockList from '@/components/BlockList.vue';
-    import PageLoader from '@/components/PageLoader.vue';
     import Block from '@/models/Block';
     import CinderApiService from '@/services/CinderApiService';
     import {SortType} from '@/models/SortType';
-    import store from '@/Store';
-    import {mapState} from 'vuex';
 
-    @Component({
-        components: {
-            BlockList,
-            PageLoader,
-        },
-        computed: mapState([
-            'isLoading',
-        ]),
-    })
+    @Component({})
     export default class HomePage extends Vue {
-        public isLoading!: boolean;
-        public blocks: Block[] | null = null;
+        public currentPage: number = 1;
+        public perPage: number = 20;
+        public total: number = 0;
+        public blocks: Block[] = [];
+        public isLoading: boolean = false;
         private api = new CinderApiService();
 
         public async created() {
-            store.commit('toggleLoader', true);
+            this.isLoading = true;
             await this.getRecentBlocks();
-            store.commit('toggleLoader', false);
+            this.isLoading = false;
+        }
+
+        public async onPageChange(page: number) {
+            this.isLoading = true;
+            this.currentPage = page;
+            await this.getRecentBlocks();
+            this.isLoading = false;
         }
 
         private async getRecentBlocks(): Promise<void> {
             try {
-                const data = await this.api.getBlocks(1, 20, SortType.Descending);
+                const data = await this.api.getBlocks(this.currentPage, this.perPage, SortType.Descending);
+                this.total = data.total < 1000 ? data.total : 1000;
                 this.blocks = data.items;
             } catch (e) {
             }
